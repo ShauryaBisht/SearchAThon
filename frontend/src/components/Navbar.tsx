cd54a6aca774ea1d33111ff38f5ac9361366080d
Shaurya Bisht <shauryabisht707@gmail.com>
2026-03-26 00:40:31 +0530

    navlink added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -37,6 +37,7 @@ export default function Navbar() {
             <NavLink to='/' className={({ isActive }) =>`transition-colors ${isActive? "text-blue-600 dark:text-blue-400 font-semibold": "text-slate-700 dark:text-slate-300 hover:text-blue-500 dark:hover:text-blue-400"}`}>Home</NavLink>
             <NavLink to='/teams'  className={({ isActive }) =>`transition-colors ${isActive? "text-blue-600 dark:text-blue-400 font-semibold": "text-slate-700 dark:text-slate-300 hover:text-blue-500 dark:hover:text-blue-400"}`}>Teams</NavLink>
             <NavLink to='/contact-us' className={({ isActive }) =>`transition-colors ${isActive? "text-blue-600 dark:text-blue-400 font-semibold": "text-slate-700 dark:text-slate-300 hover:text-blue-500 dark:hover:text-blue-400"}`}>Contact Us</NavLink>
+            {user &&<NavLink to='/my-teams' className={({ isActive }) =>`transition-colors ${isActive? "text-blue-600 dark:text-blue-400 font-semibold": "text-slate-700 dark:text-slate-300 hover:text-blue-500 dark:hover:text-blue-400"}`}>My Teams</NavLink>}
             <li><ModeToggle /></li>
           </ul>
         </nav>
